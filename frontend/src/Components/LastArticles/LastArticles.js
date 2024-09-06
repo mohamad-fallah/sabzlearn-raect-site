0c2591bfea8ce1e8a91adcc922c017757a7bfc99
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-09-06 13:22:33 +0330

    article setions

--- frontend/src/Components/LastArticles/LastArticles.js
+++ frontend/src/Components/LastArticles/LastArticles.js
@@ -1,5 +1,6 @@
 import React from "react";
-import SectionHeader from './../SectionHeader/SectionHeader'
+import ArticleBox from "../ArticleBox/ArticleBox";
+import SectionHeader from "./../SectionHeader/SectionHeader";
 
 import "./LastArticles.css";
 
@@ -7,13 +8,31 @@ export default function LastArticles() {
   return (
     <section class="articles">
       <div class="container">
+        <SectionHeader
+          title="جدیدترین مقاله ها"
+          desc="پیش به سوی ارتقای دانش"
+          btnTitle="تمامی مقاله ها"
+        />
 
-          <SectionHeader
-            title="جدیدترین مقاله ها"
-            desc="پیش به سوی ارتقای دانش"
-            btnTitle="تمامی مقاله ها"
-          />
-
+        <div class="articles__content">
+          <div class="row">
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              cover="images/blog/3.jpg"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع..."
+            />
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              cover="images/blog/3.jpg"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع..."
+            />
+            <ArticleBox
+              title="نحوه نصب کتابخانه در پایتون | آموزش نصب کتابخانه پایتون"
+              cover="images/blog/3.jpg"
+              desc="زبان پایتون هم مانند دیگر زبان­های برنامه نویسی رایج، دارای کتابخانه های مختلفی برای تسریع..."
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
