1ceaed86157d536c60ceeeba5679416e796570d5
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-09-06 12:28:48 +0330

    finish cousre and about section

---
+++ frontend/src/Components/LastCourses/LastCourses.js
@@ -0,0 +1,34 @@
+import React from "react";
+import CourseBox from "../CourseBox/CourseBox";
+import SectionHeader from "../SectionHeader/SectionHeader";
+
+import "./LastCourses.css";
+
+export default function LastCourses() {
+  return (
+    <>
+      <div class="courses">
+        <div class="container">
+          <SectionHeader
+            title="جدیدترین دوره ها"
+            desc="سکوی پرتاپ شما به سمت موفقیت"
+            btnTitle="تمامی دوره ها"
+          />
+
+          <div class="courses-content">
+            <div class="container">
+              <div class="row">
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+                <CourseBox />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/pages/Index/Index.js
+++ frontend/src/pages/Index/Index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
+import LastCourses from '../../Components/LastCourses/LastCourses'
+import AboutUs from '../../Components/AboutUs/AboutUs'
 
 import './Index.css'
 
 export default function Index() {
   return (
+    <>
     <Header />
+    <LastCourses />
+    <AboutUs/>
+    </>
   )
 }
