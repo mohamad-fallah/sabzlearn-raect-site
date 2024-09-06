32c6c415f64a1928cd2d46d00a5367d405bf368b
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-09-06 13:36:41 +0330

    finish footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
-import './styles/reset.css'
-import './styles/fonts.css'
-import './styles/variables.css'
-import './styles/helpers.css'
-import './styles/defaults.css'
+import "./styles/reset.css";
+import "./styles/fonts.css";
+import "./styles/variables.css";
+import "./styles/helpers.css";
+import "./styles/defaults.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- frontend/src/pages/Index/Index.js
+++ frontend/src/pages/Index/Index.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import Header from '../../Components/Header/Header'
-import LastCourses from '../../Components/LastCourses/LastCourses'
-import AboutUs from '../../Components/AboutUs/AboutUs'
-import PopularCourses from '../../Components/PopularCourses/PopularCourses'
-import LastArticles from '../../Components/LastArticles/LastArticles'
-import PresellCourses from '../../Components/PresellCourses/PresellCourses'
+import React from "react";
+import Header from "../../Components/Header/Header";
+import LastCourses from "../../Components/LastCourses/LastCourses";
+import AboutUs from "../../Components/AboutUs/AboutUs";
+import PopularCourses from "../../Components/PopularCourses/PopularCourses";
+import LastArticles from "../../Components/LastArticles/LastArticles";
+import PresellCourses from "../../Components/PresellCourses/PresellCourses";
+import Footer from "../../Components/Footer/Footer";
 
-import './Index.css'
+import "./Index.css";
 
 export default function Index() {
   return (
     <>
-    <Header />
-    <LastCourses />
-    <AboutUs/>
-    <PopularCourses/>
-    <PresellCourses/>
-    <LastArticles/>
+      <Header />
+      <LastCourses />
+      <AboutUs />
+      <PopularCourses />
+      <PresellCourses />
+      <LastArticles />
+      <Footer />
     </>
-  )
+  );
 }
