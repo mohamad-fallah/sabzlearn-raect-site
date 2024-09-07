5f16a1f32b0469145bf38f5e5841b4b605b4021e
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-09-07 13:17:18 +0330

    fix login big and use useReduce

--- frontend/src/Components/Form/Input.js
+++ frontend/src/Components/Form/Input.js
@@ -1,17 +1,53 @@
-import React from "react";
+import React, {useReducer} from "react";
 
 import "./Input.css";
 
 export default function Input(props) {
+
+    const inputReducer = (state , action) => {
+        switch(action.type) {
+            case "CHANGE": {
+                return {
+                    ...state,
+                    value: action.value,
+                    isValid: action.isValid
+                }
+            }
+            default: {
+                return state
+            }
+        }
+    }
+    
+    const [mainInput, dispatch] = useReducer(inputReducer, {
+        value: '',
+        isValid: false
+    })
+
+    const onChangeHandler = (event) => {
+        dispatch({
+            type: "CHANGE",
+            value: event.target.value,
+            isValid: true
+        })
+    }
+
   const element =
     props.element === "input" ? (
       <input
         type={props.type}
         placeholder={props.placeholder}
-        className={props.className}
+        className={`${props.className} ${mainInput.isValid ? 'success' : 'error'}`}
+        value={mainInput.value}
+        onChange={onChangeHandler}
       />
     ) : (
-      <textarea placeholder={props.placeholder} className={props.className} />
+      <textarea
+        placeholder={props.placeholder}
+        className={`${props.className} ${mainInput.isValid ? 'success' : 'error'}`}
+        value={mainInput.value}
+        onChange={onChangeHandler}
+      />
     );
 
   return <div>{element}</div>;
