ec299f38bcd1305c61af516e40205a5305e0d10a
Mohammadreza Fallahfaal <mfallahfaal@gmail.com>
2024-09-07 10:42:14 +0330

    finish beadCrump

--- frontend/src/pages/CourseInfo/CourseInfo.js
+++ frontend/src/pages/CourseInfo/CourseInfo.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Topbar from "./../../Components/Topbar/Topbar";
+import Navbar from "./../../Components/Navbar/Navbar";
+import Footer from "./../../Components/Footer/Footer";
 
 import './CourseInfo.css'
+import Breadcrumb from '../../Components/Breadcrumb/Breadcrumb';
 
 export default function CourseInfo() {
   return (
-    <div>CourseInfo</div>
+    <>
+      <Topbar />
+      
+      <Navbar />
+
+      <Breadcrumb
+        links={[
+          {id: 1, title: "خانه", to: ''},
+          {id: 2, title: "آموزش برنامه نویسی فرانت‌اند", to: 'category-info/frontend'},
+          {id: 3, title: "دوره متخصص جاوا اسکریپت", to: 'course-info/js-expert'},
+        ]}
+      />
+
+      <Footer />
+    </>
   )
 }
